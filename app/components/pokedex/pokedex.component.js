a3ce0c53e9af1d0ae1e7f95ccf893667e22da658
brosell1 <brosell@live.co.uk>
2018-03-22 12:13:28 +0000

    added loading bar when getting pokemon

--- app/components/pokedex/pokedex.component.js
+++ app/components/pokedex/pokedex.component.js
@@ -1,6 +1,7 @@
 class PokedexController {
   constructor(pokeService, $http) {
     this.page = false;
+    this.loading = false;
     this.allPokemon = [];
     this.searchText = '';
     this.currentPokemon = '';
@@ -11,16 +12,16 @@ class PokedexController {
 
   getAllPokemon() {
     console.log('GETTING ALL POKEMON');
-    this.pokeService
-      .getFromDatabase('//pokeapi.salestock.net/api/v2/pokemon-species')
-      .then(results => {
-        this.allPokemon = results;
-        console.log(results);
-      });
-    // this.$http.get('pokemon.json').then(results => {
-    // console.log('Json:', results);
-    // this.allPokemon = results.data;
-    // });
+    // this.pokeService
+    //   .getFromDatabase('//pokeapi.salestock.net/api/v2/pokemon-species')
+    //   .then(results => {
+    //     this.allPokemon = results;
+    //     console.log(results);
+    //   });
+    this.$http.get('pokemon.json').then(results => {
+      console.log('Json:', results);
+      this.allPokemon = results.data;
+    });
   }
 
   mapThroughEvoChain(data, evoArray = []) {
@@ -43,45 +44,54 @@ class PokedexController {
   }
 
   getOnePokemon(url) {
-    this.pokeService.getOnePokemon(url).then(data => {
-      this.pokeService.getOnePokemon(data.varieties[0].pokemon.url).then(secondaryData => {
-        this.pokeService.getOnePokemon(data.evolution_chain.url).then(evolutionData => {
-          this.mapThroughEvoChain(evolutionData.chain, [
-            {
-              name: evolutionData.chain.species.name,
-              id: evolutionData.chain.species.url.match(/(?<!\w)\d+/)[0],
-            },
-          ]);
-          this.currentPokemon = {
-            color: data.color.name,
-            evolutionChain: this.evoData,
-            flavorText: [
-              data.flavor_text_entries[1].flavor_text,
-              data.flavor_text_entries[25].flavor_text,
-            ],
-            genderRate: 'still a mystery',
-            genus: data.genera[0].genus,
-            height: secondaryData.height,
-            id: data.id,
-            number: `000${data.id}`.substr(-3),
-            name: data.name,
-            shape: data.shape.name,
-            sprite: secondaryData.sprites.front_default,
-            stats: {
-              attack: secondaryData.stats[4].base_stat,
-              spAtk: secondaryData.stats[2].base_stat,
-              speed: secondaryData.stats[0].base_stat,
-              defense: secondaryData.stats[3].base_stat,
-              spDef: secondaryData.stats[1].base_stat,
-              hp: secondaryData.stats[5].base_stat,
-            },
-            types: secondaryData.types.sort((a, b) => a.slot - b.slot).map(item => item.type.name),
-            weight: secondaryData.weight,
-          };
-          console.log(this.currentPokemon);
+    if (this.loading) {
+      console.log('Loading, please wait...');
+      return;
+    } else {
+      this.loading = true;
+      this.pokeService.getOnePokemon(url).then(data => {
+        this.pokeService.getOnePokemon(data.varieties[0].pokemon.url).then(secondaryData => {
+          this.pokeService.getOnePokemon(data.evolution_chain.url).then(evolutionData => {
+            this.mapThroughEvoChain(evolutionData.chain, [
+              {
+                name: evolutionData.chain.species.name,
+                id: evolutionData.chain.species.url.match(/(?<!\w)\d+/)[0],
+              },
+            ]);
+            this.currentPokemon = {
+              color: data.color.name,
+              evolutionChain: this.evoData,
+              flavorText: [
+                data.flavor_text_entries[1].flavor_text,
+                data.flavor_text_entries[25].flavor_text,
+              ],
+              genderRate: 'still a mystery',
+              genus: data.genera[0].genus,
+              height: secondaryData.height,
+              id: data.id,
+              number: `000${data.id}`.substr(-3),
+              name: data.name,
+              shape: data.shape.name,
+              sprite: secondaryData.sprites.front_default,
+              stats: {
+                attack: secondaryData.stats[4].base_stat,
+                spAtk: secondaryData.stats[2].base_stat,
+                speed: secondaryData.stats[0].base_stat,
+                defense: secondaryData.stats[3].base_stat,
+                spDef: secondaryData.stats[1].base_stat,
+                hp: secondaryData.stats[5].base_stat,
+              },
+              types: secondaryData.types
+                .sort((a, b) => a.slot - b.slot)
+                .map(item => item.type.name),
+              weight: secondaryData.weight,
+            };
+            console.log(this.currentPokemon);
+            this.loading = false;
+          });
         });
       });
-    });
+    }
   }
 
   goToPage(page) {
